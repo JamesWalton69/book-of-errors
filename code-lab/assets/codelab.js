5a89a0924226ecd4413e93295ec7d71c12c2e570
JamesWalton69 <155753568+JamesWalton69@users.noreply.github.com>
2025-10-12 16:28:25 +0530

    Update codelab.js

--- code-lab/assets/codelab.js
+++ code-lab/assets/codelab.js
@@ -1,1 +1,19 @@
-// Code Lab JS
+const codeFiles = [
+  { title: "Binomial Theorem", url: "https://www.playbook.com/s/files34/vAnDjk3LYdojtoPK4Yrj7s1U?assetToken=JnqSeYVsS4FfXz7Sz29DCx5S", type:"code" },
+  { title: "Question 21", url: "https://www.playbook.com/s/files34/KPnnyGUQV1QtkDqF1G3uvvNj?assetToken=bYndYSVEMNkdKWKrfKjjKwBh", type:"code" }
+];
+
+const grid = document.getElementById('codeGrid');
+
+function renderCode(files){
+  grid.innerHTML = '';
+  files.forEach(file=>{
+    const card=document.createElement('div');
+    card.className='card';
+    card.innerHTML=`<h3>${file.title}</h3>
+    <a href="${file.url}" target="_blank">Open Code File</a>`;
+    grid.appendChild(card);
+  });
+}
+
+renderCode(codeFiles);
